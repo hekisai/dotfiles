22f3039c4d989322cceff59fce0b32096e2ae11e
ohiram <ohiram@debian.debian>
2019-07-07 18:17:24 +0900

    update

---
+++ setup.sh
@@ -0,0 +1,18 @@
+make clean && \
+./configure \
+  --prefix=/usr/local/ \
+  --with-features=huge \
+  --enable-multibyte \
+  --with-x \
+  --enable-gpm \
+  --enable-cscope \
+  --enable-fontset \
+  --enable-fail-if-missing \
+  --enable-pythoninterp=dynamic \
+  --enable-perlinterp=dynamic \
+  --enable-python3interp=dynamic \
+  --enable-rubyinterp=dynamic \
+  --enable-luainterp=dynamic \
+  --enable-gui=auto \
+  --enable-gtk2-check \
+  --enable-terminal \
